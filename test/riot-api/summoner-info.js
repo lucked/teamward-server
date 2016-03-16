e34fe494cf570fb7cf9c087e055808fee103d97e
Neamar <neamar@neamar.fr>
2016-03-16 23:16:01 +0000

    Load many ranks at once

--- test/riot-api/summoner-info.js
+++ test/riot-api/summoner-info.js
@@ -53,19 +53,22 @@ describe("Summoner data", function() {
   describe('getCurrentRank()', function() {
     before(function() {
       nock('https://euw.api.pvp.net')
-        .get('/api/lol/euw/v2.5/league/by-summoner/19083089/entry')
+        .get('/api/lol/euw/v2.5/league/by-summoner/19083089,19917877/entry')
         .query(true)
         .reply(200, require('../mocks/league-entry.json'));
 
     });
 
     it("should return current rank for summoner", function(done) {
-      summonerData.getCurrentRank(19083089, 'euw', function(err, data) {
+      summonerData.getCurrentRank([19083089, 19917877], 'euw', function(err, data) {
         if(err) {
           return done(err);
         }
 
         assert.ok(data[19083089]);
+        assert.ok(data[19917877]);
+        assert.equal(data[19083089][0].tier, 'GOLD');
+        assert.equal(data[19083089][0].entries[0].division, 'I');
         assert.equal(data[19083089][0].tier, 'GOLD');
         done();
       });
