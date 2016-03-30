2cf0ce00f8f530ccd0326bdd07413ac8d4f7b3a1
Neamar <neamar@neamar.fr>
2016-03-30 16:38:49 -0700

    Added /summoner/data endpoint

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var config = require('./config');
-
 var app = express();
 
 var handlers = require('./lib').handler;
+var middlewares = require('./lib').middleware;
 
-app.get('/game/data', handlers.game.data.get);
+app.get('/summoner/data', middlewares.requireSummonerRegion, handlers.summoner.data.get);
+app.get('/game/data', middlewares.requireSummonerRegion, handlers.game.data.get);
 mongoose.connect(config.mongoUrl);
 
 module.exports = app;
