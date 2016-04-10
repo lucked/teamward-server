acb8c4bcbc131be199fcaeeb34c568300df6a752
Neamar <neamar@neamar.fr>
2016-04-10 11:45:10 -0700

    Tests fro region to platform

---
+++ test/helpers/region-to-platform.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('assert');
+var regionToPlatform = require('../../lib/helpers/region-to-platform.js');
+
+describe("regionToPlatform()", function() {
+  it('should return platfrom from region', function() {
+    assert('EUW1', regionToPlatform('euw'));
+    // Should be case insensitive
+    assert('EUN1', regionToPlatform('EUNE'));
+  });
+});
