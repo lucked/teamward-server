a3358cdd1e24d17af8b7181226a8f420829f6d6b
Neamar <neamar@neamar.fr>
2016-04-19 22:41:17 -0700

    Add opbeat monitoring

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,12 @@
 "use strict";
+if(process.env.OPBEAT_APP_ID) {
+  require('opbeat').start({
+    appId: process.env.OPBEAT_APP_ID,
+    organizationId: process.env.OPBEAT_ORGANIZATION_ID,
+    secretToken: process.env.OPBEAT_SECRET_TOKEN
+  });
+}
+
 var app = require('../app');
 
 app.listen(process.env.PORT || 3000, function() {
