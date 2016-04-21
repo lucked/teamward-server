666058944bb07e559b3ae82695caa2ac1cdfca9d
Neamar <neamar@neamar.fr>
2016-04-20 21:05:11 -0700

    Add opbeat middleware

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 "use strict";
 if(process.env.OPBEAT_APP_ID) {
-  require('opbeat').start({
+  var opbeat = require('opbeat').start({
     appId: process.env.OPBEAT_APP_ID,
     organizationId: process.env.OPBEAT_ORGANIZATION_ID,
     secretToken: process.env.OPBEAT_SECRET_TOKEN
@@ -11,6 +11,7 @@ var throng = require('throng');
 var start = function() {
   var app = require('../app');
 
+  app.use(opbeat.middleware.express());
   app.listen(process.env.PORT || 3000, function() {
     console.log('App listening on port 3000!');
   });
