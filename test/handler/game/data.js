fb877130d52126e7bd984c83af2afd248f618614
Neamar <neamar@neamar.fr>
2016-06-19 13:56:54 +0100

    Fixed indentation

--- test/handler/game/data.js
+++ test/handler/game/data.js
@@ -9,36 +9,36 @@ describe("Main server", function() {
   describe("GET /game/data", function() {
     before(function() {
       nock('https://euw.api.pvp.net')
-        .get('/api/lol/euw/v1.4/summoner/by-name/neamar')
-        .query(true)
-        .reply(200, require('../../mocks/summoner-by-name.json'))
-        .get('/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/70448430')
-        .query(true)
-        .reply(404, require('../../mocks/get-spectator-game-info-404.json'));
+      .get('/api/lol/euw/v1.4/summoner/by-name/neamar')
+      .query(true)
+      .reply(200, require('../../mocks/summoner-by-name.json'))
+      .get('/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/70448430')
+      .query(true)
+      .reply(404, require('../../mocks/get-spectator-game-info-404.json'));
     });
 
     it("should require summoner name", function(done) {
       supertest(app)
-        .get('/game/data')
-        .expect(409)
-        .expect(/summoner param/i)
-        .end(done);
+      .get('/game/data')
+      .expect(409)
+      .expect(/summoner param/i)
+      .end(done);
     });
 
     it("should require region param", function(done) {
       supertest(app)
-        .get('/game/data?summoner=neamar')
-        .expect(409)
-        .expect(/region param/i)
-        .end(done);
+      .get('/game/data?summoner=neamar')
+      .expect(409)
+      .expect(/region param/i)
+      .end(done);
     });
 
     it("should succeed with summoner and region param", function(done) {
       supertest(app)
-        .get('/game/data?summoner=neamar&region=euw')
-        // Expect 404, nocking a not in game situation
-        .expect(404)
-        .end(done);
+      .get('/game/data?summoner=neamar&region=euw')
+      // Expect 404, nocking a not in game situation
+      .expect(404)
+      .end(done);
     });
   });
 });
