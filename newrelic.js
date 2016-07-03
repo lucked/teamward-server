b3ea06da1e49b0a2b4d5cf7a62ce1642dadf6d9f
Neamar <neamar@neamar.fr>
2016-07-03 11:50:00 +0100

    Revert "Bybye newrelic"
    This reverts commit 69259eeb4e96d66a75bfc0515a6eec163b487d54.

---
+++ newrelic.js
@@ -0,0 +1,13 @@
+exports.config = {
+  agent_enabled: "NEW_RELIC_LICENSE_KEY" in process.env,
+  app_name: "Teamward",
+  capture_params: true,
+  apdex_t: 0.250,
+  error_collector: {
+    // We need to disable the error collector to let Opbeat retrieve the stacktrace on uncaughtException
+    enabled: false,
+
+    // We ignore common 4XX errors. By default they trigger alarms on newrelic, but on a REST API they're pretty standard and should not be considered errors.
+    ignore_status_codes: [401, 403, 404, 405, 409, 410, 413, 429],
+  }
+};
