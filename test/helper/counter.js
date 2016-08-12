4111d2519e6a2369f544e58d8e877596748b351c
Neamar <neamar@neamar.fr>
2016-08-12 20:07:14 +0100

    Helper seems to be working, test suite can be improved IMO

--- test/helper/counter.js
+++ test/helper/counter.js
@@ -11,8 +11,10 @@ describe.only("Counter helper", function() {
     };
   }
   function buildMatchup(id, winRate) {
+    // For test purposes, id and names are the same
     return {
       id: id,
+      name: id,
       winRate: winRate || 50
     };
   }
@@ -23,7 +25,7 @@ describe.only("Counter helper", function() {
       _id: id,
       name: id,
       getMatchups: function() {
-        return matchups;
+        return matchups || [];
       }
     };
   }
@@ -39,8 +41,7 @@ describe.only("Counter helper", function() {
     });
   });
 
-
-  it("should return the best matchup from the list", function(done) {
+  it("should return one line for each rolesChampion", function(done) {
     var masteredChampions = [
       buildAPIChampion("illaoi")
     ];
@@ -60,12 +61,61 @@ describe.only("Counter helper", function() {
         return done(err);
       }
 
-      assert.equal(res.length, 1);
+      assert.equal(res.length, 3);
       assert.equal(res[0].champion.id, "graves");
       assert.equal(res[0].champion.name, "graves");
       assert.equal(res[0].counter.id, "illaoi");
       assert.equal(res[0].winRate, 55);
+      assert.equal(res[1].champion.id, "teemo");
+      assert.equal(res[1].counter.id, "illaoi");
+      // Worst counter ever :p
+      assert.equal(res[1].winRate, 30);
+      assert.equal(res[2].champion.id, "illaoi");
+      // No counter for this one, make sure it's still null
+      assert.equal(res[2].counter, null);
+      assert.equal(res[2].winRate, null);
 
+      done();
+    });
+  });
+
+  it("should find the best counter from the list", function(done) {
+    var masteredChampions = [
+      buildAPIChampion("illaoi"),
+      buildAPIChampion("singed"),
+    ];
+
+    var rolesChampions = [
+      buildDbChampion("graves", [
+        buildMatchup("singed", 55),
+        buildMatchup("illaoi", 45)
+      ]),
+      buildDbChampion("illaoi", [
+        buildMatchup("singed")
+      ]),
+      buildDbChampion("singed", [
+        buildMatchup("illaoi")
+      ]),
+    ];
+
+    counterHelper(rolesChampions, masteredChampions, "TOP", function(err, res) {
+      if(err) {
+        return done(err);
+      }
+
+      assert.equal(res.length, 3);
+      assert.equal(res[0].champion.id, "graves");
+      assert.equal(res[0].champion.name, "graves");
+      assert.equal(res[0].counter.id, "illaoi");
+      assert.equal(res[0].winRate, 55);
+      assert.equal(res[1].champion.id, "illaoi");
+      assert.equal(res[1].counter.id, "singed");
+      assert.equal(res[1].winRate, 50);
+      assert.equal(res[2].champion.id, "singed");
+      assert.equal(res[2].counter.id, "illaoi");
+      assert.equal(res[2].winRate, 50);
+
+      done();
     });
   });
 });
