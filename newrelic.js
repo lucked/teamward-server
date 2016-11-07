a4164436caff7d8f8c3d8ad6cee841cc8583999b
Neamar <neamar@neamar.fr>
2016-11-07 15:36:56 +0000

    Enable NR error collection

--- newrelic.js
+++ newrelic.js
@@ -5,7 +5,7 @@ exports.config = {
   apdex_t: 0.250,
   error_collector: {
     // We need to disable the error collector to let Opbeat retrieve the stacktrace on uncaughtException
-    enabled: false,
+    enabled: true,
 
     // We ignore common 4XX errors. By default they trigger alarms on newrelic, but on a REST API they're pretty standard and should not be considered errors.
     ignore_status_codes: [401, 403, 404, 405, 409, 410, 413, 429],
