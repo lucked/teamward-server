cbf0bec5235e1e318560c71920c9812bf1df546c
Neamar <neamar@neamar.fr>
2016-11-16 19:58:34 +0000

    Removec HttpCache

--- test/setup.js
+++ test/setup.js
@@ -1,20 +1,17 @@
 'use strict';
-var mongoose = require("mongoose");
 var nock = require("nock");
 
 var ddragon = require("../lib/ddragon");
 var cache = require('../lib/riot-api/cache');
 
 
-before(function cleanHttpCaches(done) {
-  mongoose.model('HttpCache').remove({}, done);
-});
-
-beforeEach(function cleanLocalCache() {
+beforeEach(function cleanCaches() {
+  // API cache
   cache.lruCache.reset();
-});
 
-beforeEach(function cleanNock() {
+  // DDragon cache
   ddragon._cache = {};
+
+  // Previous nocks
   nock.cleanAll();
 });
