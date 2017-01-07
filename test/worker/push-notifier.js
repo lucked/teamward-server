cbf9bf718f8cad375074ad992f0166e62fbd8c9b
Neamar <neamar@neamar.fr>
2017-01-07 13:20:46 +0000

    Test: notification is sent when in game

--- test/worker/push-notifier.js
+++ test/worker/push-notifier.js
@@ -3,8 +3,12 @@
 var assert = require("assert");
 var mongoose = require("mongoose");
 var async = require("async");
+var sinon = require("sinon");
 var nock = require('nock');
+
 var pushNotifier = require('../../lib/worker/push-notifier.js');
+var gameData = require('../../lib/helper/game-data.js');
+
 
 var Token = mongoose.model('Token');
 
@@ -23,8 +27,12 @@ describe("Worker: pushNotifier", function() {
     return token;
   };
 
-  var saveDummyToken = function(cb) {
-    getDummyToken().save(cb);
+  var saveDummyToken = function(t, cb) {
+    if(!cb) {
+      cb = t;
+      t = getDummyToken();
+    }
+    t.save(cb);
   };
 
   it("should do nothing when player is not in game", function(done) {
@@ -45,5 +53,45 @@ describe("Worker: pushNotifier", function() {
         cb();
       }
     ], done);
+  });
+
+  it("should send a notification when player is in game", function(done) {
+    var fakeGameData = require('../mocks/mocks/custom_get-spectator-game-info.json');
+
+    async.waterfall([
+      saveDummyToken,
+      function(token, count, cb) {
+        nock('https://euw.api.pvp.net')
+          .get('/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/' + token.summonerId)
+          .query(true)
+          .reply(200, fakeGameData);
+
+        sinon.stub(gameData, 'buildExternalGameData', function() {});
+        sinon.stub(pushNotifier.gcm, 'send', function(message, cb) {
+          assert.equal(message.registration_id, token.token);
+          assert.equal(message['data.gameId'], fakeGameData.gameId);
+
+          process.nextTick(function() {
+            cb(null, "fakemessageid");
+          });
+        });
+
+        pushNotifier({testing: true}, cb);
+      },
+      function(tokenCounter, tokenNotifiedCounter, cb) {
+        assert.equal(tokenCounter, 1);
+        assert.equal(tokenNotifiedCounter, 1);
+        sinon.assert.calledOnce(gameData.buildExternalGameData);
+        sinon.assert.calledOnce(pushNotifier.gcm.send);
+
+        cb();
+      }
+    ], function(err) {
+      // Always restore functionality
+      gameData.buildExternalGameData.restore();
+      pushNotifier.gcm.send.restore();
+
+      done(err);
+    });
   });
 });
