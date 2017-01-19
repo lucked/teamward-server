4630f9a83f9b07c6174145a35f4e45a755813a6f
Neamar <neamar@neamar.fr>
2017-01-19 19:41:51 +0000

    Updated clean-redis in case of errors

--- bin/clean-redis.js
+++ bin/clean-redis.js
@@ -6,14 +6,16 @@ var config = require('../config/');
 
 
 kue.createQueue({
-  jobEvents: false, // Do not send individual job events (useful for our case with many small tasks)
   redis: config.redisUrl
 });
 
 kue.Job.range(0, 100000, 'asc', function(err, jobs) {
   async.eachLimit(jobs, 30, function(j, cb) {
     j.remove(cb);
-  }, function() {
+  }, function(err) {
+    if(err) {
+      throw err;
+    }
     console.log("Cleaned redis.");
     process.exit();
   });
