84892500b863bbf94f539d37048bfd5f5659d75f
Neamar <neamar@neamar.fr>
2017-01-21 17:55:12 +0000

    Cheap dynos. I'm ashamed

--- bin/server.js
+++ bin/server.js
@@ -6,22 +6,35 @@ var common = require('./_common');
 
 require('heroku-self-ping')(process.env.APP_URL);
 
-var throng = require('throng');
+if(process.env.CHEAP_WORKER_DYNO) {
+  // Using a legacy web dyno to run the worker
+  require('./worker-push-notifier.js');
 
-var start = function() {
-  var app = require('../app');
-
-  if(common.opbeat) {
-    app.use(common.opbeat.middleware.express());
-  }
-
-  var port = process.env.PORT || 3000;
-  app.listen(port, function() {
-    log('App listening on port ' + port + '!');
+  var express = require('express');
+  var app = express();
+  app.get('/', function(req, res) {
+    res.send({cheap_dyno: true});
   });
-};
+  app.listen(process.env.PORT || 3000);
+}
+else {
+  var throng = require('throng');
 
-var concurrency = process.env.WEB_CONCURRENCY || 1;
+  var start = function() {
+    var app = require('../app');
 
-log("Initializing app with concurrency=" + concurrency);
-throng(concurrency, start);
+    if(common.opbeat) {
+      app.use(common.opbeat.middleware.express());
+    }
+
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+      log('App listening on port ' + port + '!');
+    });
+  };
+
+  var concurrency = process.env.WEB_CONCURRENCY || 1;
+
+  log("Initializing app with concurrency=" + concurrency);
+  throng(concurrency, start);
+}
