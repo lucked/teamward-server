c78bd38ec94d5608ce9f2f0da7eebb8810f168b1
Neamar <neamar@neamar.fr>
2017-01-29 21:32:28 +0000

    Add one more test for already existing associations

--- test/model/premade.js
+++ test/model/premade.js
@@ -82,5 +82,30 @@ describe("Premade model", function() {
         }
       ], done);
     });
+
+    it("should skip existing premades associations", function(done) {
+      async.waterfall([
+        function add(cb) {
+          var premade = new Premade();
+          premade._id = "euw:1";
+          premade.premades = [2];
+          premade.save(cb);
+        },
+        function save(premade, count, cb) {
+          Premade.savePremadesToDB({100: [[1, 2, 3]]}, "euw", cb);
+        },
+        function load(cb) {
+          Premade.find({}).sort({_id: 1}).exec(cb);
+        },
+        function check(premades, cb) {
+          assert.equal(premades.length, 3);
+          assert.deepEqual(premades[0].toObject(), {_id: "euw:1", premades: [2, 3]});
+          assert.deepEqual(premades[1].toObject(), {_id: "euw:2", premades: [1, 3]});
+          assert.deepEqual(premades[2].toObject(), {_id: "euw:3", premades: [1, 2]});
+
+          cb();
+        }
+      ], done);
+    });
   });
 });
