ab3557fa776df816d94c9f52d8e498b07284f6d7
Neamar <neamar@neamar.fr>
2017-02-07 22:08:10 -0800

    Trying pm2 and keymetrics

--- bin/server.js
+++ bin/server.js
@@ -6,22 +6,13 @@ var common = require('./_common');
 
 require('heroku-self-ping')(process.env.APP_URL);
 
-var throng = require('throng');
+var app = require('../app');
 
-var start = function() {
-  var app = require('../app');
+if(common.opbeat) {
+  app.use(common.opbeat.middleware.express());
+}
 
-  if(common.opbeat) {
-    app.use(common.opbeat.middleware.express());
-  }
-
-  var port = process.env.PORT || 3000;
-  app.listen(port, function() {
-    log('App listening on port ' + port + '!');
-  });
-};
-
-var concurrency = process.env.WEB_CONCURRENCY || 1;
-
-log("Initializing app with concurrency=" + concurrency);
-throng(concurrency, start);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  log('App listening on port ' + port + '!');
+});
