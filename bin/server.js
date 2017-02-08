39d3c598da75dfe36b3430297bd5c901279a3c7f
Neamar <neamar@neamar.fr>
2017-02-07 22:20:17 -0800

    Revert "Trying pm2 and keymetrics"
    This reverts commit ab3557fa776df816d94c9f52d8e498b07284f6d7.

--- bin/server.js
+++ bin/server.js
@@ -6,13 +6,22 @@ var common = require('./_common');
 
 require('heroku-self-ping')(process.env.APP_URL);
 
-var app = require('../app');
+var throng = require('throng');
 
-if(common.opbeat) {
-  app.use(common.opbeat.middleware.express());
-}
+var start = function() {
+  var app = require('../app');
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  log('App listening on port ' + port + '!');
-});
+  if(common.opbeat) {
+    app.use(common.opbeat.middleware.express());
+  }
+
+  var port = process.env.PORT || 3000;
+  app.listen(port, function() {
+    log('App listening on port ' + port + '!');
+  });
+};
+
+var concurrency = process.env.WEB_CONCURRENCY || 1;
+
+log("Initializing app with concurrency=" + concurrency);
+throng(concurrency, start);
