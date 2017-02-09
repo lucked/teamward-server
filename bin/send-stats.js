202e83a7dbdac99fd9d874baccfd431699d28c90
Neamar <neamar@neamar.fr>
2017-02-08 21:11:02 -0800

    Function to generate stats

---
+++ bin/send-stats.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var async = require("async");
+var mongoose = require("mongoose");
+var assert = require("assert");
+
+require('../app.js');
+var metricTracker = require('../lib/metric-tracker.js');
+
+
+var queue = async.queue(function(task, cb) {
+  console.log(task);
+  metricTracker(task.name, task.value);
+  setTimeout(cb, 110);
+}, 1);
+
+async.parallel([
+  function regions(cb) {
+    mongoose.model('Token').aggregate([
+    {
+      $group: {
+        _id: "$region",
+        count: {$sum: 1}
+      }
+    }]).read('secondaryPreferred').exec(function(err, res) {
+      assert.ifError(err);
+
+      res.forEach(function(regionStats) {
+        console.log(regionStats);
+        // queue.push({
+        //   name: "Tokens.Counter." + regionStats._id.toUpperCase(),
+        //   value: regionStats.count
+        // });
+      });
+
+      cb();
+    });
+  },
+  function premades(cb) {
+    mongoose.model('Premade').count(function(err, count) {
+      assert.ifError(err);
+      queue.push({
+        name: "Premades.Counter",
+        value: count
+      });
+      cb();
+    });
+  },
+  function premadesSize(cb) {
+    mongoose.model('Premade').aggregate([
+    {
+      $project: {
+        size: {$size: '$premades'}
+      }
+    },
+    {
+      $group: {
+        _id: "1",
+        avg: {$avg: "$size"}
+      }
+    }]).read('secondaryPreferred').exec(function(err, res) {
+      assert.ifError(err);
+      queue.push({
+        name: "Premades.AverageLength",
+        value: res[0].avg
+      });
+      cb();
+    });
+  }
+], function(err) {
+  if(err) {
+    throw err;
+  }
+
+  queue.drain = function() {
+    process.exit(0);
+  };
+});
