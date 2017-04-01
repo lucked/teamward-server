0c5c51cb9cfa848edc50ee19883ad717fa83422b
Neamar <neamar@neamar.fr>
2017-04-01 11:03:03 +0100

    Changed my mind, will store matches directly in cache

--- test/worker/download-games.js
+++ test/worker/download-games.js
@@ -11,9 +11,6 @@ describe("pushNotifier worker", function() {
   beforeEach(function clearDB(done) {
     mongoose.model('Match').remove({}, done);
   });
-  beforeEach(function clearDB(done) {
-    mongoose.model('ApiMatch').remove({}, done);
-  });
 
   it("should download match data and format them properly", function(done) {
     done = recorder.useNock(this, done);
@@ -23,15 +20,11 @@ describe("pushNotifier worker", function() {
       region: 'euw'
     };
 
-    downloadGamesWorker.workerFunction(job, function(err, res) {
+    downloadGamesWorker.workerFunction(job, function(err, match) {
       assert.ifError(err);
-      var match = res[0][0];
-      var apiMatch = res[1][0];
       assert.equal(match._id, job.region + ":" + job.id);
       assert.equal(match.tier, "BRONZE");
       assert.equal(match.teams[1].players[2].role, "MID");
-
-      assert.equal(match._id, apiMatch._id);
 
       done();
     });
