9c9033e95b8c8fa0919310c0e98d3be89d22374d
Neamar <neamar@neamar.fr>
2017-04-21 10:43:10 +0100

    Flag teamward users

--- test/helper/game-data.js
+++ test/helper/game-data.js
@@ -3,6 +3,7 @@
 var nock = require("nock");
 var mongoose = require("mongoose");
 var async = require("async");
+var rarity = require("rarity");
 
 var recorder = require('../mocks/recorder.js');
 var assert = require('assert');
@@ -12,6 +13,7 @@ var gameData = require('../../lib/helper/game-data');
 describe("Game data", function() {
   var Premade = mongoose.model('Premade');
   var Game = mongoose.model('Game');
+  var Token = mongoose.model('Token');
 
   beforeEach(function(done) {
     Premade.remove({}, done);
@@ -19,6 +21,10 @@ describe("Game data", function() {
 
   beforeEach(function(done) {
     Game.remove({}, done);
+  });
+
+  beforeEach(function(done) {
+    Token.remove({}, done);
   });
 
   it("should return current game data", function(done) {
@@ -51,23 +57,24 @@ describe("Game data", function() {
     this.timeout(40000);
     done = recorder.useNock(this, done);
     var fakeGameData = require('../mocks/mocks/custom_get-spectator-game-info.json');
-    gameData.buildExternalGameData(fakeGameData, 'euw', function(err, data) {
-      assert.ifError(err);
 
-      assert.equal(data.map_id, 11);
-
-      // Remove all nocks
-      nock.cleanAll();
-
-      gameData.buildExternalGameData(fakeGameData, 'euw', function(err, data) {
-        assert.ifError(err);
-
+    async.waterfall([
+      function callOnce(cb) {
+        gameData.buildExternalGameData(fakeGameData, 'euw', cb);
+      },
+      function callTwice(data, cb) {
         assert.equal(data.map_id, 11);
 
-        done();
-      });
+        // Remove all nocks
+        nock.cleanAll();
 
-    });
+        gameData.buildExternalGameData(fakeGameData, 'euw', cb);
+      },
+      function verify(data, cb) {
+        assert.equal(data.map_id, 11);
+        cb();
+      }
+    ], done);
   });
 
   it("should guess premades data", function(done) {
@@ -114,6 +121,35 @@ describe("Game data", function() {
       function checkGames(games, cb) {
         assert.ok(games.length > 0);
         assert.equal(games[0].players.length, 10);
+
+        cb();
+      }
+    ], done);
+  });
+
+  it("should flag teamward users", function(done) {
+    this.timeout(40000);
+    done = recorder.useNock(this, done);
+
+    var fakeGameData = require('../mocks/mocks/custom_get-spectator-game-info-premade.json');
+
+    async.waterfall([
+      function addToken(cb) {
+        var token = new Token();
+        token.summonerId = fakeGameData.participants[0].summonerId;
+        token.region = 'euw';
+        token.token = 'fake';
+        token.summonerName = 'fake';
+        token.summonerProfileId = 1;
+
+        token.save(rarity.slice(1, cb));
+      },
+      function build(cb) {
+        gameData.buildExternalGameData(fakeGameData, 'euw', cb);
+      },
+      function verify(data, cb) {
+        assert.ok(data.teams[0].players[0].teamwardUser);
+        assert.ok(!data.teams[0].players[1].teamwardUser);
 
         cb();
       }
