c9a5d1741bdcec5debebc0cdc693660bf7390f95
Neamar <neamar@neamar.fr>
2017-05-01 19:17:17 +0100

    Updated max queries

--- lib/model/sql/queries/achievements/longest_game.sql
+++ lib/model/sql/queries/achievements/longest_game.sql
@@ -1,8 +1,10 @@
 SELECT
-    matches.id, MAX(duration)
+    matches.id, duration
 FROM
     matches
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
         AND region = 'euw')
+ORDER BY duration DESC
+LIMIT 1
--- lib/model/sql/queries/achievements/max_cs.sql
+++ lib/model/sql/queries/achievements/max_cs.sql
@@ -1,8 +1,10 @@
 SELECT
-    matches.id, MAX(cs)
+    matches.id, cs
 FROM
     matches
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
         AND region = 'euw')
+ORDER BY cs DESC
+LIMIT 1
