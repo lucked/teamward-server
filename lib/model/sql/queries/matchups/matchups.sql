a1a05ba8a08ab179a5404904138a3781353ba4b8
Neamar <neamar@neamar.fr>
2017-05-01 19:12:41 +0100

    Matchups without a VIEW

--- lib/model/sql/queries/matchups/matchups.sql
+++ lib/model/sql/queries/matchups/matchups.sql
@@ -1,19 +1,48 @@
 SELECT
-    Champion1, Champion2, role, SUM(nbgame), SUM(nbwin) / SUM(nbgame) * 100 AS winrate
+    RAW_DATA.Champion1,
+    RAW_DATA.Champion2,
+    RAW_DATA.role,
+    sum(RAW_DATA.nbgame) as nbgame,
+    sum(RAW_DATA.nbwin)/sum(RAW_DATA.nbgame) * 100 as winrate
 FROM
-    (SELECT
-        champion1, champion2, role, nbwin, nbgame
+(
+ SELECT
+        player1.champion_id AS champion1,
+        player2.champion_id AS champion2,
+        player1.role AS role,
+        SUM(CASE
+            WHEN (matches.winner = player1.team_id) THEN 1
+            ELSE 0 END) AS nbwin,
+        COUNT(0) AS nbgame
     FROM
-        Matchup
-UNION ALL
-SELECT
-        champion2,
-            champion1,
-            role,
-            (nbgame - nbwin) AS nbwin,
-            nbgame
+        matches
+        INNER JOIN matches_participants player1 ON player1.match_id = matches.id AND player1.team_id = 100
+        INNER JOIN matches_participants player2 ON player2.match_id = matches.id AND player2.team_id = 200 AND player1.role = player2.role
+    GROUP BY
+        player1.champion_id,
+        player2.champion_id,
+        player1.role
+    UNION ALL
+    SELECT
+        player2.champion_id AS champion1,
+        player1.champion_id AS champion2,
+        player1.role AS role,
+        SUM(CASE
+            WHEN (matches.winner = player2.team_id) THEN 1
+            ELSE 0 END) AS nbwin,
+        COUNT(0) AS nbgame
     FROM
-        Matchup) Gathered_data
-GROUP BY champion1 , champion2 , role
-HAVING SUM(nbgame) > 100
-ORDER BY winrate ASC;
+        matches
+        INNER JOIN matches_participants player1 ON player1.match_id = matches.id AND player1.team_id = 100
+        INNER JOIN matches_participants player2 ON player2.match_id = matches.id AND player2.team_id = 200 AND player1.role = player2.role
+    GROUP BY
+        player1.champion_id,
+        player2.champion_id,
+        player1.role
+) RAW_DATA
+GROUP BY
+    RAW_DATA.champion1,
+    RAW_DATA.champion2,
+    RAW_DATA.role
+HAVING
+    SUM(RAW_DATA.nbgame) > 100;
