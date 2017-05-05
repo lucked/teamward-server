d228fd2c2fd8db84341a6d930880880d7b843232
Neamar <neamar@neamar.fr>
2017-05-05 11:37:37 +0200

    Redo matchups without a temporary table

--- lib/model/sql/queries/matchups/matchups.sql
+++ lib/model/sql/queries/matchups/matchups.sql
@@ -1,48 +1,19 @@
 SELECT
-    RAW_DATA.Champion1,
-    RAW_DATA.Champion2,
-    RAW_DATA.role,
-    sum(RAW_DATA.nbgame) as nbgame,
-    sum(RAW_DATA.nbwin)/sum(RAW_DATA.nbgame) * 100 as winrate
-FROM
-(
- SELECT
         player1.champion_id AS champion1,
+        champions1.name AS champion1name,
         player2.champion_id AS champion2,
+        champions2.name AS champion2name,
         player1.role AS role,
-        SUM(CASE
-            WHEN (matches.winner = player1.team_id) THEN 1
-            ELSE 0 END) AS nbwin,
-        COUNT(0) AS nbgame
-    FROM
-        matches
-        INNER JOIN matches_participants player1 ON player1.match_id = matches.id AND player1.team_id = 100
-        INNER JOIN matches_participants player2 ON player2.match_id = matches.id AND player2.team_id = 200 AND player1.role = player2.role
-    GROUP BY
-        player1.champion_id,
-        player2.champion_id,
-        player1.role
-    UNION ALL
-    SELECT
-        player2.champion_id AS champion1,
-        player1.champion_id AS champion2,
-        player1.role AS role,
-        SUM(CASE
-            WHEN (matches.winner = player2.team_id) THEN 1
-            ELSE 0 END) AS nbwin,
-        COUNT(0) AS nbgame
-    FROM
-        matches
-        INNER JOIN matches_participants player1 ON player1.match_id = matches.id AND player1.team_id = 100
-        INNER JOIN matches_participants player2 ON player2.match_id = matches.id AND player2.team_id = 200 AND player1.role = player2.role
-    GROUP BY
-        player1.champion_id,
-        player2.champion_id,
-        player1.role
-) RAW_DATA
+        SUM(CASE WHEN (matches.winner = player1.team_id) THEN 1 ELSE 0 END) as nb_wins,
+        COUNT(0) AS nb_games
+FROM matches
+INNER JOIN matches_participants player1 ON player1.match_id = matches.id
+INNER JOIN matches_participants player2 ON player2.match_id = matches.id AND player2.team_id != player1.team_id AND player1.role = player2.role
+LEFT JOIN champions champions1 ON player1.champion_id = champions1.id
+LEFT JOIN champions champions2 ON player2.champion_id = champions2.id
 GROUP BY
-    RAW_DATA.champion1,
-    RAW_DATA.champion2,
-    RAW_DATA.role
+    player1.champion_id,
+    player2.champion_id,
+    player1.role
 HAVING
-    SUM(RAW_DATA.nbgame) > 100;
+    nb_games > 100;
