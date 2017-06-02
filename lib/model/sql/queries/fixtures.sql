576e5066ad27054a866f19221ce52b4795b6d6d2
Neamar <neamar@neamar.fr>
2017-06-02 15:20:32 +0200

    Queue all the games!

--- lib/model/sql/queries/fixtures.sql
+++ lib/model/sql/queries/fixtures.sql
@@ -15,7 +15,7 @@ CREATE TABLE `matches_participants` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `match_id` bigint(20) unsigned NOT NULL,
   `team_id` tinyint(3) unsigned NOT NULL,
-  `summoner_id` int(10) unsigned NOT NULL,
+  `summoner_id` int(10) unsigned NULL,
   `role` enum('?','TOP','JUNGLE','MID','CARRY','SUPPORT') NOT NULL,
   `champion_id` int(10) unsigned NOT NULL,
   `kills` int(10) unsigned NOT NULL,
