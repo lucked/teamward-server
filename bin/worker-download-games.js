b44921086929e2dd0af935538984c2c540fb6ef2
Neamar <neamar@neamar.fr>
2017-06-05 16:51:14 +0100

    Do not use throng on download games

--- bin/worker-download-games.js
+++ bin/worker-download-games.js
@@ -1,12 +1,10 @@
 "use strict";
 require('newrelic');
 
-var throng = require('throng');
-
 require('./_common');
 require('../app');
 
 // Start worker
 if(!process.env.DISABLE_DOWNLOAD_GAMES) {
-  throng(1, require('../lib/worker/download-games'));
+  require('../lib/worker/download-games')();
 }
