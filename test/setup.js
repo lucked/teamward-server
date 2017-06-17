0067f836ee7c55db2d2107cf7da7064450e334f9
Neamar <neamar@neamar.fr>
2017-06-17 12:01:33 +0100

    Prelaod ddragon once for all tests

--- test/setup.js
+++ test/setup.js
@@ -4,10 +4,18 @@ var nock = require("nock");
 var ddragon = require("../lib/ddragon");
 var cache = require('../lib/riot-api/cache.js');
 
-beforeEach(function cleanCaches() {
-  // DDragon cache
-  ddragon._cache = {};
+// Fake ddragon data for all tests
+before(function loadDdragon() {
+  ddragon._cache = {
+    '/realms/euw.json': require('./mocks/mocks/custom_ddragon_realms_euw.json'),
+    '/cdn/7.12.1/data/en_US/champion.json': require('./mocks/mocks/custom_ddragon_champion.json'),
+    '/cdn/7.12.1/data/en_US/item.json': require('./mocks/mocks/custom_ddragon_item.json'),
+    '/cdn/7.12.1/data/en_US/summoner.json': require('./mocks/mocks/custom_ddragon_summoner.json'),
+  };
+});
 
+
+beforeEach(function cleanCaches() {
   // Previous nocks
   nock.cleanAll();
 });
@@ -15,3 +23,4 @@ beforeEach(function cleanCaches() {
 beforeEach(function cleanRedisCache(done) {
   cache.connection.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", 0, "cache.*", done);
 });
+
