0a5e3e4cf020b894d071b30e3c74e5ec3b41f74c
Matthieu Bacconnier <neamar@neamar.fr>
2017-06-24 22:34:38 +0100

    Update first.sql

--- lib/model/sql/queries/achievements/first.sql
+++ lib/model/sql/queries/achievements/first.sql
@@ -5,15 +5,15 @@ FROM
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
-        AND region = 'euw')
-) AS count, (SELECT
+        AND matches_participants.region = 'euw')
+) AS nbgames, (SELECT
     COUNT(0)
 FROM
     matches
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
-        AND region = 'euw'
+        AND matches_participants.region = 'euw'
         AND first_blood=TRUE)
 ) AS first_blood, (SELECT
     COUNT(0)
@@ -22,7 +22,7 @@ FROM
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
-        AND region = 'euw'
+        AND matches_participants.region = 'euw'
         AND first_tower=TRUE)
 ) AS first_tower, (SELECT
     COUNT(0)
@@ -31,6 +31,6 @@ FROM
         INNER JOIN
     matches_participants ON (match_id = matches.id
         AND summoner_id = 70448430
-        AND region = 'euw'
+        AND matches_participants.region = 'euw'
         AND first_inhibitor=TRUE)
 ) AS first_inhibitor
